b942604b1cfae4fb3dfb594d780e13f877f82e28
Manuel Kniep <m.kniep@web.de>
2014-06-17 09:19:56 +0200

    add aggregate test for negative values

--- test/sql/aggregates_test.sql
+++ test/sql/aggregates_test.sql
@@ -1,2 +1,4 @@
 SELECT SUM(val::inthstore) FROM (VALUES ('foo=>25, bar=>10'),('foo=>25, bar =>5, baz=>15'))t(val);
+SELECT SUM(val::inthstore) FROM (VALUES ('foo=>25, bar=>10'),('foo=>25, bar =>-5, baz=>15'))t(val);
 SELECT SUM(val::floathstore) FROM (VALUES ('foo=>2.5, bar=>1.0'),('foo=>2.5, bar =>0.5, baz=>1.5'))t(val);
+SELECT SUM(val::floathstore) FROM (VALUES ('foo=>2.5, bar=>1.0'),('foo=>2.5, bar =>-0.5, baz=>1.5'))t(val);
